202651eb8fb3f06a973a6800afaee5748b2a45bd
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2023-04-24 08:52:17 +0900

    second commit

--- LikeLion230423/ContentView.swift
+++ LikeLion230423/ContentView.swift
@@ -2,25 +2,59 @@
 //  ContentView.swift
 //  LikeLion230423
 //
-//  Created by yeon I on 2023/04/23.
+//  Created by yeon on 2023/04/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    let coffees :[coffee] = [
+        .init(name:"아이스 아메리카노", imageName:"", description: ""),
+        .init(name:"자바 프라프치노", imageName:"", description: ""),
+        .init(name:"라벤더 라떼", imageName:"", description: ""),
+        .init(name:"모카", imageName:"", description: ""),
+        .init(name:"돌체라떼", imageName:"", description: ""),
+        .init(name:"아이스 아메리카노", imageName:"", description: ""),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            //swift ui -> ios 13
+            //navigation stack - > ios  14 부터
+            List(coffees, id: \.name){ coffee in
+                NavigationLink(destination: DetailView(coffee: coffee)){
+                    HStack {
+                        VStack(alignment: .leading, spacing: 6){
+                            Text(coffee.name)
+                                .font(.subheadline)
+                            
+                            
+                        }
+                        Spacer()
+                        
+                        
+                        Image(coffee.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100)
+                        
+                    }
+                    .frame(height:120)
+                }
+                .navigationTitle("커피하우스")
+            }
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
+    //cmd + +or -
+    
+    
 }
--- LikeLion230423/DetailView.swift
+++ LikeLion230423/DetailView.swift
@@ -5,4 +5,29 @@
 //  Created by yeon I on 2023/04/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct DetailView: View {
+    let coffee :coffee
+    
+    var body: some View {
+        VStack{
+            Image(coffee.imageName)
+                .resizable()
+                .scaledToFit()
+            Text(coffee.name)
+                .font(.largeTitle)
+            
+            Text(coffee.description)
+            Link(destination: URL(string: "https://www.naver.com/")!){
+                Text("웹에서 자세히 보기 ", systemImage: "globe")
+            }
+        }
+    }
+}
+
+struct DetailView_Previews:PreviewProvider{
+    static var previews: some View{
+        
+    }
+}
